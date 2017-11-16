defc974d246931c13c8b695d38fef01b8e5f1485
samuelpack <packmac2@gmail.com>
2017-11-16 10:43:32 -0500

    body-parser practice

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -50,16 +50,18 @@ UserSchema.virtual('fullName').get(function() {
 // Use a pre-save middleware to hash the password
 UserSchema.pre('save', function(next) {
     if (this.password) {
-        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.salt = Buffer.fromy(crypto.randomBytes(16).toString('base64'), 'base64');
+        // this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        // crypto.pbkdf2Sync(password, new Buffer(this.salt,'binary'),10000,64,'sha1').toString('base64');
         this.password = this.hashPassword(this.password);
-    }
+    }    
 
     next();
 });
 
 // Create an instance method for hashing a password
 UserSchema.methods.hashPassword = function(password) {
-    return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha512').toString('base64');
+    return crypto.pbkdf2Sync(password, this.salt, 10000, 64, 'sha1').toString('base64');
 };
 
 // Create an instance method for authenticating user
--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,3 +1,6 @@
+//the module.exports approach exposes a single (anonymous?) function
+
+
 module.exports = function(app) {
     const index = require ('../controllers/index.server.controller');
     app.get('/', index.render);
